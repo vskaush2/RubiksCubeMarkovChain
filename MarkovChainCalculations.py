08c5b98866e9f811cd7dad2bb67e1dcc9cc4c656
vskaush2 <vskaush2@illinois.edu>
2021-12-26 23:35:27 -0800

    Updated documentation and exposition in RubiksCubeMarkovChainDemo.ipynb

--- MarkovChainCalculations.py
+++ MarkovChainCalculations.py
@@ -11,7 +11,7 @@ class MarkovChainCalculations:
         self.transition_matrix = MarkovGraph(self.n, True).transition_matrix
 
 
-    def compute_chain_distribution(self,t):
+    def compute_chain_distribution(self, t):
         directory = '{}D/Chain_Distributions'.format(self.n)
         try:
             os.makedirs(directory)
@@ -24,12 +24,12 @@ class MarkovChainCalculations:
             pi_t = sc.sparse.load_npz(file_path)
 
         else:
-            if t==0:
+            if t == 0:
                 N = self.transition_matrix.shape[0]
                 I = sc.sparse.identity(N)
                 pi_t = I.getcol(0) # pi_0 is the first basis vector in R^N (corresponding to the constant random variable equal to the identity permutation)
 
-            else:
+            if t > 0:
                 pi_t = self.transition_matrix * self.compute_chain_distribution(t-1) # Recursive Sparse Matrix Vector Calculation
 
             if np.isclose(pi_t.data.sum(), 1.0):
@@ -39,14 +39,14 @@ class MarkovChainCalculations:
 
         return pi_t
 
+
     def compute_current_total_variation_distances_df(self):
         N = self.transition_matrix.shape[0]
         t=len(os.listdir('{}D/Chain_Distributions'.format(self.n)))
         pis = Parallel(n_jobs=t, verbose=0, prefer='threads')(delayed(self.compute_chain_distribution)(j) for j in range(t))
         current_total_variation_distances_df=pd.DataFrame(index=range(t))
-
-        current_total_variation_distances_df['Total Variation Distance to Uniform']= [0.5 * np.linalg.norm(pi.data - 1 / N, 1) + 0.5 * (N - len(pi.data)) / N
-                                                                                      for pi in pis]
+        current_total_variation_distances_df['Total Variation Distance to Discrete Uniform Distribution'] \
+            = [0.5 * np.linalg.norm(pi.data - 1 / N, 1) + 0.5 * (N - len(pi.data)) / N for pi in pis]
         current_total_variation_distances_df.index.rename("Chain Distribution",inplace=True)
         return current_total_variation_distances_df
 
